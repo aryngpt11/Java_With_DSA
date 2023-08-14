090ee0c22a3e12dbfcdba0740638fc214438d154
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-14 15:43:41 +0530

    Arithmetic

---
+++ Basics/ArithmeticOperator.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int a=20;
+        int b=10;
+        int ad=a+b;
+        int s=a-b;
+        int d=a/b;
+        int m=a%b;
+        System.out.println(ad);
+        System.out.println(s);
+        System.out.println(d);
+        System.out.println(m);
+    }
+    
+}
