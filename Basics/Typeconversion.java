3f22b34e189367723e1db02091622625dd9ab073
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-14 15:34:01 +0530

    java

---
+++ Basics/Typeconversion.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class Typeconversion {
+    public static void main(String[] args) {
+        byte b=10;
+        short a=20;
+        char c='a';
+
+        byte d=(byte) (b*2);
+        double f=a+b+c+d;
+        System.out.println(f);
+    }
+    
+}
