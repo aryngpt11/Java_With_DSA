5885c2a44f47efc863d079263bac20298d8d9c09
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-14 14:59:47 +0530

    variables

---
+++ Basics/VAriabless.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class VAriabless {
+    public static void main(String[] args) {
+        int a=11;
+        int b=27;
+        System.out.println(a);
+        System.out.println(b);
+        String Name="Aryan";
+        String Name1="Arya";
+        System.out.println(Name);
+        System.out.println(Name1);
+    }
+    
+}
