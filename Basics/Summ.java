43f255d7a976a03bb294e84c7379865885405abd
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-14 15:34:10 +0530

    Sum

---
+++ Basics/Summ.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class Summ {
+    public static void main(String[] args) {
+        int a=10;
+        int b=25;
+        int c=a+b;
+        System.out.println(c);
+    }
+    
+}
