77cb93ab5970668426d181484a95514b4db2a383
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-15 14:12:03 +0530

    terneary

---
+++ ConditionalStatements/Ternary_Oper.java
@@ -0,0 +1,8 @@
+public class Ternary_Oper {
+    public static void main(String[] args) {
+        int a=22;
+        String type=(a%2==0)?"even":"odd";
+        System.out.println(type);
+    }
+    
+}
