4982dcc202a0f03a557e5ca9715d82472e66e77b
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-15 12:40:04 +0530

    BAsics

---
+++ Basics/Pattern2.java
@@ -0,0 +1,11 @@
+package Basics;
+public class Pattern2{
+    public static void main(String[] args) {
+        for(int i=5;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ Basics/UnaryOperator.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class UnaryOperator {
+    public static void main(String[] args) {
+        int a=10;
+        int b=a++;
+        int c=++a;
+        System.out.println(a+"value of a");
+        System.out.println(b+"value of b");
+        System.out.println(c+"value of c");
+    }
+    
+}
