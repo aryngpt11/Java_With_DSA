802095156d43473eb26a43724ade5ce1941662a8
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-15 14:25:21 +0530

    Switch

---
+++ ConditionalStatements/Ternary_opr2.java
@@ -0,0 +1,8 @@
+public class Ternary_opr2 {
+    public static void main(String[] args) {
+        int marks=31;
+        String s=(marks>=33)?"Pass":"Fail";
+        System.out.println(s);
+    }
+    
+}
---
+++ SwitchStatements/First_Eg.java
@@ -0,0 +1,20 @@
+package SwitchStatements;
+
+public class First_Eg {
+    public static void main(String[] args) {
+        int num=5;
+        switch(num){
+            case 1 : System.out.println("Pizza");
+                        break;
+            case 2 : System.out.println("Samosa");
+                        break;
+            case 3 : System.out.println("Momos");
+                        break;
+            case 4 : System.out.println("Cold Drink");
+                        break;
+            default:System.out.println(" we wake up");
+
+        }
+    }
+    
+}
