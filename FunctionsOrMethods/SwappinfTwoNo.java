b35ba69098c9f1903ad5d4955982b09a7df76845
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-17 00:05:42 +0530

    Swapping two num

---
+++ FunctionsOrMethods/SwappinfTwoNo.java
@@ -0,0 +1,20 @@
+package FunctionsOrMethods;
+
+public class SwappinfTwoNo {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        System.out.println("the value of a before swapping: "+a);
+        System.out.println("the value of b before swapping: "+b);
+        int temp=a;
+
+//swapping two numbers without using function/methods
+
+        a=b;
+        b=temp;
+        System.out.println("the value of a after swapping: "+a);
+        System.out.println("the value of b after swapping: "+b);
+
+    }
+    
+}
