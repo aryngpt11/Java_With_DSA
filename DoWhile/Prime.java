e285171dd0b92fdc7104eba0eda1d9911d86a49f
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-16 14:44:47 +0530

    loop

--- DoWhile/Prime.java
+++ DoWhile/Prime.java
@@ -7,7 +7,9 @@ public class Prime {
         int n=sc.nextInt();
         boolean isPrime=true;
         //for(int=2;i<=n-1;i++)
-        for(int i=2;i<=n/2;i++){
+        for(int i=2;i<=Math.sqrt(n);i++)
+        //for(int i=2;i<=n/2;i++)
+        {
             if(n%i==0){
                 isPrime=false;
             }
