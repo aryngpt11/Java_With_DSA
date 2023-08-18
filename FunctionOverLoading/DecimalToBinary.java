05ecaca9f64c4c23d1b4850283cd3e7793df8f93
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-18 10:51:18 +0530

    DecimalToBinary

---
+++ FunctionOverLoading/DecimalToBinary.java
@@ -0,0 +1,21 @@
+package FunctionOverLoading;
+
+public class DecimalToBinary {
+    public static void dectoBin(int n){
+        int mynum=n;
+        int pow=0;
+        int binnum=0;
+        while(n>0){
+            int rem=n%2;
+            binnum=binnum+(rem*(int)Math.pow(10,pow));
+            pow++;
+            n=n/2;
+        }
+        System.out.println("binary form of "+mynum +"is "+binnum);
+    }
+    public static void main(String[] args) {
+        dectoBin(88);
+    
+}
+    
+}
