d056926b40275a129f1c6a241303b3efb154849f
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-18 15:38:46 +0530

    Butterfly

---
+++ Pattern/ButterFlyPattern.java
@@ -0,0 +1,36 @@
+package Pattern;
+public class ButterFlyPattern{
+    public static void butterly(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //2nd half
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        butterly(5);
+        
+    }
+}
