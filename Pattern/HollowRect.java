e1c85df3eed1ba451cb232eae3de8bfa306fe979
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-18 11:54:04 +0530

    Hollow

---
+++ Pattern/HollowRect.java
@@ -0,0 +1,24 @@
+package Pattern;
+
+public class HollowRect {
+    public static void Hollow_rect(int rows,int cols){
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=cols;j++){
+            if(i==1||i==rows|| j==1 ||j==cols){
+                System.out.print("*"+" ");
+            }
+            else{
+                System.out.print(" "+" ");
+            }
+        }
+        System.out.println();
+    }
+
+
+}
+    public static void main(String[] args) {
+        Hollow_rect(10,5 );
+        
+    }
+    
+}
