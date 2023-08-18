dba918b93c6c4f26c8f2bb029467cfac8a2684e3
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-18 21:40:56 +0530

    Pattern

---
+++ Pattern/RhombusPattern.java
@@ -0,0 +1,25 @@
+package Pattern;
+import java.util.Scanner;
+public class RhombusPattern {
+    public static void Rhombus(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n=sc.nextInt();
+        Rhombus(n);
+        sc.close();
+        
+    }
+    
+}
+ 
