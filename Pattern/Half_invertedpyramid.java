b55ef5a59c9b1c11e8e59d86c3a799ee14240175
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-18 14:04:12 +0530

    FullndHalfPyramid

---
+++ Pattern/Half_invertedpyramid.java
@@ -0,0 +1,22 @@
+package Pattern;
+
+public class Half_invertedpyramid {
+    public static void Half_Pyramid(int n){
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+    public static void main(String[] args) {
+        Half_Pyramid(5);
+        
+    }
+    
+}
