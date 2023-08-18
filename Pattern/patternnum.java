ad9e4cb04172121c0506900630593e40b746f251
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-18 14:24:55 +0530

    patternfun

--- Pattern/patternnum.java
+++ Pattern/patternnum.java
@@ -2,12 +2,13 @@ package Pattern;
 public class patternnum {
     public static void main(String[] args) {
         int n=10;
-        int count=0;
+        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(j+count+" ");
+                System.out.print(j+" ");
+                
             }
-            count++;
+            //count++;
             System.out.println();
         }
     }
