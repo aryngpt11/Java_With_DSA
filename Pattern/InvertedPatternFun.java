4303aac70e9ea2e85f5422ddb9674f88de81d26f
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-18 14:46:21 +0530

    Inverted

---
+++ Pattern/InvertedPatternFun.java
@@ -0,0 +1,19 @@
+package Pattern;
+
+public class InvertedPatternFun {
+    public static void InvertedWithCounting(int n){
+        int num=55;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(num--+" ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        InvertedWithCounting(10);
+        
+    }
+    
+}
