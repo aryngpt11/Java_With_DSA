5fbf29d3f43094016c6f644166b91f92a4925e25
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-20 00:28:53 +0530

    Largest

---
+++ Array/FindingLargest.java
@@ -0,0 +1,24 @@
+package Array;
+import java.util.*;
+public class FindingLargest {
+    public static int largest(int num[]){
+        int large=Integer.MIN_VALUE;
+        int small=Integer.MAX_VALUE;
+        for(int i=0;i<num.length;i++){
+            if(large<num[i]){
+                large=num[i];
+            }
+            if(small>num[i]){
+                small=num[i];
+            }
+        }
+        System.out.println("Smallest no:"+small);
+        return large;
+    }
+    public static void main(String[] args) {
+        int num[]={1,2,6,3,5};
+        System.out.println(largest(num));
+        
+    }
+    
+}
