69fd0a0a4e63b7a8ca5e68cc1231888798df1eab
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-19 21:18:57 +0530

    Array

---
+++ Array/LinearSearch.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int key=10;
+        int marks[]={4,7,9,6,5,8,11,10,20,30};
+        for(int i=0;i<marks.length;i++){
+            if(key==marks[i]){
+                
+            }
+            else{
+                System.out.println("not found");
+            }
+
+        }
+        
+
+        }
+        
+    }
+    
+
