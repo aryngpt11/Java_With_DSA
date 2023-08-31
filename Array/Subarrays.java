3073192c0af54c3b1ceec766b466d1f8ac9e1042
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-31 20:22:34 +0530

    subarray

--- Array/Subarrays.java
+++ Array/Subarrays.java
@@ -4,7 +4,7 @@ public class Subarrays {
     public static void prsubarr(int num[]){
         for(int i=0;i<num.length;i++){
             int start=i;
-            for(int j=i; j<num.length;j++){
+            for(int j=i+1; j<num.length;j++){
                 int end=j;
                 for(int k=start;k<=end;k++){
                     System.out.print(num[k]+" ");
