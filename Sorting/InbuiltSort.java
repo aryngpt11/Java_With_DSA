8a57feb6b7777298d106b2661206b8ef0a332fed
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-09-02 22:52:34 +0530

    Inbuilt sorting

--- Sorting/InbuiltSort.java
+++ Sorting/InbuiltSort.java
@@ -1,5 +1,6 @@
 package Sorting;
 import java.util.Arrays;
+import java.util.Collections;
 public class InbuiltSort {
     /* public static void printarr(int arr[]){
         for(int i=0;i<arr.length;i++){
@@ -8,13 +9,30 @@ public class InbuiltSort {
         System.out.println();
     } */
     public static void main(String[] args) {
+        //this code is for priny=ting array in the ascending order
         int arr[]={30,3,43,2,1,45,123,11};
         Arrays.sort(arr);
+        System.out.println("the given array in ascending order: ");
+        /* Arrays.sort(arr,0,5); */
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ,");
         }
         System.out.println();
-        /* printarr(arr); */
+        
+        //this code is for printing array in descending order
+
+        Integer arrr[]={30,3,43,2,1,45,123,11};
+        //Arrays.sort(arrr,0,5,Collections.reverseOrder());
+        Arrays.sort(arrr,Collections.reverseOrder());
+        System.out.println("the given array in descending order: ");
+        for(int i=0;i<arrr.length;i++){
+            System.out.print(arrr[i]+" ,");
+        }
+        System.out.println();
+
+        
+        
     }
+    
 }
  
--- Sorting/Insertionsort.java
+++ Sorting/Insertionsort.java
@@ -1,5 +1,10 @@
 package Sorting;
 
-public class Insertionsort {
-    
+public class Insertionsort { 
+    public static void insertionsort(int arr[]){
+         
+    }
+    public static void main(String[] args) {
+        int arr[]={5,4,1,3,2};
+    }
 }
