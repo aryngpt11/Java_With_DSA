94ab62f0e699091d63396b63ac68e09795ccf835
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-09-02 23:00:21 +0530

    Insertion sort

--- Sorting/Insertionsort.java
+++ Sorting/Insertionsort.java
@@ -2,9 +2,22 @@ package Sorting;
 
 public class Insertionsort { 
     public static void insertionsort(int arr[]){
-         
+        for(int i=1;i<arr.length;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            while(prev>=0 && arr[prev]> curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1]=curr;
+        }
+        for(int j=0;j<arr.length;j++){
+            System.out.print(arr[j]+" ");
+        }
+        System.out.println();
     }
     public static void main(String[] args) {
         int arr[]={5,4,1,3,2};
+        insertionsort(arr);
     }
 }
