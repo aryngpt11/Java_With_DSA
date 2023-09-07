faf7f250b119ebeeb84bf43b66aa5282ec1ba915
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-09-07 18:37:32 +0530

    2DArray

--- Two_D_Array/Creation.java
+++ Two_D_Array/Creation.java
@@ -1,6 +1,22 @@
 package Two_D_Array;
 import java.util.Scanner;
 public class Creation {
+    public static boolean Search(int matrix[][],int key){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if (matrix[i][j]==key){
+                    System.out.println("found");
+                    return true;
+                }
+                
+
+            }
+            
+        }
+        System.out.println("not found");
+        return false;
+
+    }
     public static void main(String[] args) {
         int matrix[][]=new int[3][3];
         //int n=3,m=3; or
@@ -11,15 +27,11 @@ public class Creation {
                 matrix[i][j]=sc.nextInt();
             }
         }
+        
+        Search(matrix, 5);
 
         //output
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                System.out.print(matrix[i][j]+" ");
-
-            }
-            System.out.println();
-        }
+        
         sc.close();
     }
     
