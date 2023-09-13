b471369e2cde945cddf6d372987ef732d10d06d7
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-09-13 20:50:04 +0530

    Searching

--- Two_D_Array/Searching_in_mat.java
+++ Two_D_Array/Searching_in_mat.java
@@ -20,8 +20,11 @@ public class Searching_in_mat {
      }
     public static void main(String[] args) {
         int m[][]={{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
-        int key=70;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of key: ");
+        int key=sc.nextInt();
         System.out.println(Searching(m, key));
+        sc.close();
         
     }
     
