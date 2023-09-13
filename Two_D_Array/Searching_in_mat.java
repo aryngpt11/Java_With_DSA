ed3d968d00adf036246cfc1251463d2d9d2e29a9
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-09-13 20:47:45 +0530

    SeaRCHING

---
+++ Two_D_Array/Searching_in_mat.java
@@ -0,0 +1,28 @@
+package Two_D_Array;
+import java.util.Scanner;
+public class Searching_in_mat {
+     public static boolean Searching(int m[][], int key){
+    
+        for(int i=0;i<m.length;i++){
+            
+            for(int j=0;j<m.length;j++){
+                if(key==m[i][j]){
+                   return true;
+
+                }
+                
+            }
+            
+           
+        }
+        return false;
+
+     }
+    public static void main(String[] args) {
+        int m[][]={{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
+        int key=70;
+        System.out.println(Searching(m, key));
+        
+    }
+    
+}
