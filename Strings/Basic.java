942ac03961865db79938166a279d07efad86ebb3
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-09-14 10:32:42 +0530

    BAsic

--- Strings/Basic.java
+++ Strings/Basic.java
@@ -1,8 +1,8 @@
 package Strings;
-import java.util.Scanner;
+//import java.util.*;
 public class Basic {
     public static void main(String[] args) {
-        char arr[]={'a','b','c','d'};
+       /*  char arr[]={'a','b','c','d'};
         String str="abcd";
         //or
         String str1=new String("xyz");
@@ -13,7 +13,9 @@ public class Basic {
         System.out.println(arr);
         System.out.println(str);
         System.out.println(str1);
-        sc.close();
+        sc.close(); */
+        String fullname="Aryan Gupta";
+        System.out.println(fullname.length());
     }
     
 }
