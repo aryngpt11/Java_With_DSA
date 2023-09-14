3de622c0607f17db30f81a2fe948baf45cd3726d
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-09-14 11:42:57 +0530

    substring

---
+++ Strings/Substringg.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class Substringg {
+    public static String substring(String str,int si ,int ei){
+        String substr="";
+        for(int i=si; i<ei;i++){
+            substr +=str.charAt(i);
+        }
+        return substr;
+    }
+    public static void main(String[] args) {
+        //substring
+        String str="Hello World";
+        System.out.println(substring(str, 0, 5));
+    }
+    
+}
