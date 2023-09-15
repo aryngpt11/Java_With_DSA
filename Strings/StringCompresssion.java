e8e37b800de087a67f81296e34c51444650e42e9
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-09-15 10:44:05 +0530

    String

--- Strings/StringCompresssion.java
+++ Strings/StringCompresssion.java
@@ -2,10 +2,24 @@ package Strings;
 
 public class StringCompresssion {
     public static String compress(String str){
-        String newStr
+        String newstr= "";
+        
+        for(int i=0;i<str.length();i++){
+            Integer count=1;
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newstr+=str.charAt(i);
+            if(count>1){
+                newstr+=count.toString();
+            }
+        }
+        return newstr;
     }
     public static void main(String[] args) {
         String str="aaaabbbcccdddd";
+        System.out.println(compress(str));
     }
     
 }
