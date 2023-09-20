c5aa6e73eca62a14346563505a0949adf29bb039
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-09-21 01:01:27 +0530

    bw

--- BitManipulation/AndOper.java
+++ BitManipulation/AndOper.java
@@ -3,7 +3,9 @@ package BitManipulation;
 public class AndOper {
     public static void main(String[] args) {
         System.out.println(5&6);
-        System.out.println(5|6);
+        System.out.println(5|6);//or
+        System.out.println(5^6);//xor
+        System.out.println(~5);//binary one's complement
     }
     
 }
