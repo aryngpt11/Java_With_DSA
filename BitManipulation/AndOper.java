093fc42035caf19b2d6f53310d335ff570921dd5
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-09-21 00:57:38 +0530

    BitWise

---
+++ BitManipulation/AndOper.java
@@ -0,0 +1,13 @@
+package BitManipulation;
+
+public class AndOper {
+    public static void main(String[] args) {
+        System.out.println(5&6);
+        System.out.println(5|6);
+    }
+    
+}
+
+
+
+
