0499dd29c89f416e1aad478087cffb3ba45d10dd
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-09-21 11:36:57 +0530

    operator

--- BitManipulation/AndOper.java
+++ BitManipulation/AndOper.java
@@ -6,6 +6,8 @@ public class AndOper {
         System.out.println(5|6);//or
         System.out.println(5^6);//xor
         System.out.println(~5);//binary one's complement
+        System.out.println(5<<2);//left
+        System.out.println(5>>2);//right
     }
     
 }
