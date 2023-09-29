f323381894687fbe717b238cadd4eb6412a60a12
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-09-29 22:30:58 +0530

    getterand setter

---
+++ OOPS/Getttnsett.java
@@ -0,0 +1,33 @@
+package OOPS;
+
+public class Getttnsett {
+    public static void main(String[] args) {
+        Penn p = new Penn();
+
+        p.setColor("blue");
+        System.out.println(p.getColor());
+        p.setTip(5);
+        System.out.println(p.getTip());
+    }
+}
+
+class Penn {
+    private String color;
+    private int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    void setTip(int tip) {
+        this.tip = tip;
+    }
+}
