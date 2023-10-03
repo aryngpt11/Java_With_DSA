2851c042b7b61f861bff40f5398fd5379b5431cc
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-10-03 18:09:58 +0530

    copy

--- OOPS/CopyConst.java
+++ OOPS/CopyConst.java
@@ -6,16 +6,20 @@ public class CopyConst {
         s1.name = "Aryan";
         s1.roll = 123;
         s1.password = "abcd";
+        s1.marks[0]=100;
+        s1.marks[1]=90;
+        s1.marks[2]=80;
         
         // Use the copy constructor to create s2
         Studdent s2 = new Studdent(s1); 
-        
-        // Modify s2's password
         s2.password = "xyz";
-        
-        // Print s1 and s2
+        s1.marks[2]=70;
         System.out.println( s1.name + ", " + s1.roll + ", " + s1.password);
         System.out.println( s2.name + ", " + s2.roll + ", " + s2.password);
+        for (int i=0;i<3;i++){
+            System.out.println(s2.marks[i]);
+        }
+        
     }
 }
 
@@ -23,14 +27,22 @@ class Studdent {
     String name;
     int roll;
     String password;
+    int marks[];
 
     // Copy constructor
     Studdent(Studdent s1) {
+        marks=new int[3];
         this.name = s1.name;
         this.roll = s1.roll;
-        this.password = s1.password;
+        this.marks=s1.marks;
+        
     }
     Studdent(){
+        marks=new int[3];
         System.out.println("this is a constructor");
+    }
+    Studdent(String name){
+        marks=new int[3];
+        this.name=name;
     }
 }
