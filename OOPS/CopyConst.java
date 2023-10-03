63d29d564455bdd4aef097ee085014274525f82b
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-10-03 18:13:25 +0530

    copy constructor

--- OOPS/CopyConst.java
+++ OOPS/CopyConst.java
@@ -29,14 +29,26 @@ class Studdent {
     String password;
     int marks[];
 
-    // Copy constructor
-    Studdent(Studdent s1) {
+    // Shallow Copy constructor
+    /* Studdent(Studdent s1) {
         marks=new int[3];
         this.name = s1.name;
         this.roll = s1.roll;
         this.marks=s1.marks;
         
+    } */
+    
+    //deep copy constructor
+    Studdent(Studdent s1){
+        marks=new int[3];
+        this.name = s1.name;
+        this.roll = s1.roll;
+        for(int i=0;i<marks.length;i++){
+            this.marks[i]=s1.marks[i];
+        }
+
     }
+
     Studdent(){
         marks=new int[3];
         System.out.println("this is a constructor");
