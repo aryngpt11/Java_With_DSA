9bad6dacaf84a9181e1c62030308d0ab52924a6f
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-10-03 19:41:46 +0530

    Inheritance

--- OOPS/Innheritancce.java
+++ OOPS/Innheritancce.java
@@ -2,8 +2,12 @@ package OOPS;
 
 public class Innheritancce {
     public static void main(String[] args) {
-        Fish shark=new Fish();
-        shark.eat();
+       /*  Fish shark=new Fish();
+        shark.eat(); */
+        Dog d=new Dog();
+        d.eat();
+        d.legs=4;
+        System.out.println(d.legs);
         
     }
 }
@@ -17,10 +21,16 @@ class Animal{
         System.out.println("breathes");
     }
 }
+class Mammals extends Animal{
+    int legs;
+}
+class Dog extends Mammals{
+    String breed;
+}
 //Derived Class
-class Fish extends Animal{
+/* class Fish extends Animal{
     int fins;
     void swim(){
         System.out.println("Swims in water");
     }
-}
+} */
