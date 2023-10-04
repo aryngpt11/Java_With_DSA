91a921c79c28ecc575e4607ef741258e6771ad4a
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-10-04 22:34:06 +0530

    static

---
+++ OOPS/StaticcKeyw.java
@@ -0,0 +1,30 @@
+package OOPS;
+
+public class StaticcKeyw {
+   public static void main(String[] args) {
+    Sttuudent s1=new Sttuudent();
+    s1.schoolName="GPS";
+    s1.name="Aryan";
+    System.out.println(s1.getName());
+    Sttuudent s2=new Sttuudent();
+    System.out.println(s2.schoolName);
+    
+   } 
+}
+class Sttuudent{
+    String name;
+    int roll;
+    String schoolName;
+    void setName(String name){
+        this.name=name;
+    }
+    String getName(){
+        return this.name;
+    }
+    void setRoll(int roll){
+        this.roll=roll;
+    }
+    int getRoll(){
+        return this.roll;
+    }
+}
