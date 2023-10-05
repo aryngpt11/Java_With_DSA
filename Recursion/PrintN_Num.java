8c91d6b1d6c67b3ed2739b8ef8f124b0ec374341
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-10-05 15:41:15 +0530

    decreasing

---
+++ Recursion/PrintN_Num.java
@@ -0,0 +1,20 @@
+package Recursion;
+import java.util.Scanner;
+public class PrintN_Num {
+    public static void printDec(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n+" ");
+        printDec(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of n: ");
+        int n=sc.nextInt();
+        printDec(n);
+        sc.close();
+    }
+    
+}
