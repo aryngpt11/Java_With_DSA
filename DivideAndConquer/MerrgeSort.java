ec6f640e6579f38a4ad8d74a7931ba145fa0beb0
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-10-06 12:04:06 +0530

    mergeSort

---
+++ DivideAndConquer/MerrgeSort.java
@@ -0,0 +1,53 @@
+package DivideAndConquer;
+
+public class MerrgeSort {
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void mergeSort(int arr[], int si,int ei){
+        if(si >=ei){
+            return;
+        }
+        int mid=si+(ei-si)/2;
+        mergeSort(arr, si, mid);
+        mergeSort(arr,mid+1, ei);
+        merge(arr, si, mid, ei);
+    }
+    public static void merge(int arr[], int si, int mid, int ei){
+        int temp[]=new int[ei-si+1];
+        int i=si;
+        int j=mid+1;
+        int k=0;
+        while(i<=mid &&j<=ei){
+            if(arr[i]<arr[j]){
+                temp[k]=arr[i];
+                i++;
+            }
+            else{
+                temp[k]=arr[j];
+                j++; 
+            }
+            k++;
+        }
+        //left part
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        //right part
+        while(j<=ei){
+            temp[k++]=arr[j++];
+        }
+        for(k=0,i=si; k<temp.length; k++,i++){
+            arr[i]=temp[k];
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={6,3,9,5,2,8};
+        printArr(arr);
+        mergeSort(arr, 0, arr.length-1);
+        printArr(arr);
+    }
+}
