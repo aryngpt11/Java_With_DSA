3446b75b6bcceea7b3e62ec439b3f2c159b3a80d
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-10-08 19:56:35 +0530

    permutation

---
+++ Backtracking/Permutationn.java
@@ -0,0 +1,21 @@
+package Backtracking;
+
+public class Permutationn {
+    public static void findPermutation(String str, String ans){
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        
+        for(int i=0; i<str.length();i++){
+            char curr=str.charAt(i);
+           String newstr=str.substring(0, i)+str.substring(i+1);
+            findPermutation(newstr, ans+curr);
+        }
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        findPermutation(str, "");
+    }
+    
+}
