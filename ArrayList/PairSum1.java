05b233a184d05929562639b8bee5d8785412ae58
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-10-10 13:39:48 +0530

    pairsum

--- ArrayList/PairSum1.java
+++ ArrayList/PairSum1.java
@@ -1,7 +1,9 @@
 package ArrayList;
 import java.util.ArrayList;
 public class PairSum1 {
-   public static boolean pairssum1(ArrayList<Integer>list,int target){
+
+    //brute force  O(n*n)
+   /* public static boolean pairssum1(ArrayList<Integer>list,int target){
     for(int i=0;i<list.size();i++){
         for(int j=i+1;j<list.size();j++){
             if(list.get(i)+list.get(j)==target){
@@ -10,14 +12,34 @@ public class PairSum1 {
         }
     }
     return false;
-   } 
+   }  */
+
+   //two pointer approach    O(n)
+   public static boolean pairssum1(ArrayList<Integer>list,int target){
+    int lp=0;
+    int rp=list.size()-1;
+    while(lp!=rp){
+         if(list.get(lp)+list.get(rp)==target){
+            return true;
+         }
+         if(list.get(lp)+list.get(rp)<target){
+            lp++;
+         }
+         else{
+            rp--;
+         }
+    }
+    return false;
+
+
+   }
     public static void main(String[] args) {
         ArrayList<Integer>list=new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
-        list.add(5);
+        list.add(5); 
         list.add(6);
         int target=8;
         System.out.println(pairssum1(list, target));
