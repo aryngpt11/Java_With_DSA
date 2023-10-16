03cf9f23fe136bb593526b8b3a89019a3e9d1f2d
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-10-16 11:43:20 +0530

    remove cycle

--- LinkedList/Palindromee.java
+++ LinkedList/Palindromee.java
@@ -95,6 +95,30 @@ public class Palindromee {
         }
         return false;
     }
+    public static void removeCycle(){
+        Node slow=head;
+        Node fast=head;
+        boolean cycle=false;
+        while(fast !=null && fast.next !=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
+                cycle=true;
+                break;
+            }
+        }
+        if(cycle==false){
+            return;
+        }
+        slow=head;
+        Node prev=null;
+        while(slow!= fast){
+            prev =fast;
+            slow=slow.next;
+            fast=fast.next;
+        }
+        prev.next=null;
+    }
 
     public static void main(String[] args) {
        /*  Palindromee ll = new Palindromee();
@@ -107,9 +131,12 @@ public class Palindromee {
         ll.print();
         System.out.println(ll.checkPalindrome());*/
         head=new Node(1);
-        head.next=new Node(2);
+        Node temp=new Node(2);
+        head.next=temp; 
         head.next.next=new Node(3);
-        head.next.next.next=head;
+        head.next.next.next=temp;
+        System.out.println(isCycle());
+        removeCycle();
         System.out.println(isCycle());
     } 
 }
