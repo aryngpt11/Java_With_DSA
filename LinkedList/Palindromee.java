a52b8609f33ac981f617711b66f8ac825563f7db
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-10-16 22:36:35 +0530

    MergeSort ll

--- LinkedList/Palindromee.java
+++ LinkedList/Palindromee.java
@@ -119,6 +119,55 @@ public class Palindromee {
         }
         prev.next=null;
     }
+    private Node getMid(Node head){
+        Node slow=head;
+        Node fast=head.next;
+        while(fast !=null && fast.next !=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    private Node merge(Node head1, Node head2){
+        Node mergedLL=new Node(-1);
+        Node temp=mergedLL;
+        while(head1!=null &&head2!=null){
+            if(head1.data<=head2.data){
+                temp.next=head1;
+                head1=head1.next;
+                temp=temp.next;
+            }
+            else{
+                temp.next=head2;
+                head2=head2.next;
+                temp=temp.next;
+            }
+        }
+        while(head1 !=null){
+            temp.next=head1;
+            head1=head1.next;
+            temp=temp.next;
+
+        }
+        while(head2 !=null){
+            temp.next=head2;
+            head2=head2.next;
+            temp=temp.next;
+
+        }
+        return mergedLL.next;
+    }
+    public Node mergeSort(Node head){
+        if(head==null || head.next==null){
+            return head;
+        }
+        Node mid=getMid( head);
+        Node rightHead=mid.next;
+        mid.next=null;
+        Node newLeft=mergeSort(head);
+        Node newRight=mergeSort(rightHead);
+        return merge(newLeft,newRight);
+    }
 
     public static void main(String[] args) {
        /*  Palindromee ll = new Palindromee();
@@ -130,7 +179,7 @@ public class Palindromee {
 
         ll.print();
         System.out.println(ll.checkPalindrome());*/
-        head=new Node(1);
+        /* head=new Node(1);
         //head.next=new Node(2);
         Node temp=new Node(2);
         head.next=temp; 
@@ -139,6 +188,16 @@ public class Palindromee {
         //head.next.next.next=head;
         System.out.println(isCycle());
         removeCycle();
-        System.out.println(isCycle());
+        System.out.println(isCycle()); */
+        Palindromee pl=new Palindromee();
+        pl.addFirst(1);
+        pl.addFirst(2);
+        pl.addFirst(3);
+        pl.addFirst(4);
+        pl.addFirst(5);
+        pl.print();
+        pl.head=pl.mergeSort(pl.head);
+        pl.print();
+        
     } 
 }
